8fe318d406e8bfe13685073a1ce5cedc8545f2b4
Yuhui Lyu <yuhuilvlu@gmail.com>
2020-03-25 14:07:01 -0400

    updates of p3

--- p3.m
+++ p3.m
@@ -18,11 +18,22 @@ figure('Name',"5x5 Median filter result (5 iterations)"),imshow(med_image);
 %% Apply 5x5 mean filter on grayscale image
 fzero = zeros(size(f));
 % 1 Iteration
-mean_image = mean5x5(f, fzero, size(f, 1), size(f,2));
+mean_image = mean5x5(f);
 figure('Name',"5x5 Mean filter result (1 iteration)"),imshow(mean_image);
 
 % 5 Iterations
 for i = 1:4
-    mean_image = mean5x5(mean_image, fzero, size(f, 1), size(f,2));
+    mean_image = mean5x5(mean_image);
 end
 figure('Name',"5x5 Mean filter result (5 iterations)"),imshow(mean_image);
+
+%% 5x5 sigma filter (sigma = 20)
+% 1 Iteration
+sigma_image = sigma5x5(f, 20);
+figure('Name',"5x5 sigma filter result with sigma 20 (1 iteration)"),imshow(sigma_image);
+
+% 5 Iterations
+for i = 1:4
+   sigma_image = sigma5x5(sigma_image, 20);
+end
+figure('Name',"5x5 sigma filter result with sigma 20 (5 iterations)"),imshow(sigma_image);
