e70366ca9d79e5ed352e014631e5706ebee7a5c1
Yuhui Lyu <yuhuilvlu@gmail.com>
2020-03-25 11:59:34 -0400

    updates

--- p3.m
+++ p3.m
@@ -2,13 +2,11 @@
 clear all;
 % Load the image
 f = imread('disk.gif');
-% Convert RGB image to grayscale image
-g = rgb2gray(f);
-figure('Name',"Original Grayscale image"),imshow(g);
+figure('Name',"Original Grayscale image"),imshow(f);
 
 %% Apply built-in median filter on grayscale image
 % 1 Iteration
-med_image = medfilt2(g, [5,5]);
+med_image = medfilt2(f, [5,5]);
 figure('Name',"5x5 Median filter result (1 iteration)"),imshow(med_image);
 
 % 5 Iterations
@@ -18,13 +16,13 @@ end
 figure('Name',"5x5 Median filter result (5 iterations)"),imshow(med_image);
 
 %% Apply 5x5 mean filter on grayscale image
-gzero = zeros(size(g));
+fzero = zeros(size(f));
 % 1 Iteration
-mean_image = mean5x5(g, gzero, size(g, 1), size(g,2));
+mean_image = mean5x5(f, fzero, size(f, 1), size(f,2));
 figure('Name',"5x5 Mean filter result (1 iteration)"),imshow(mean_image);
 
 % 5 Iterations
 for i = 1:4
-    mean_image = mean5x5(mean_image, gzero, size(g, 1), size(g,2));
+    mean_image = mean5x5(mean_image, fzero, size(f, 1), size(f,2));
 end
 figure('Name',"5x5 Mean filter result (5 iterations)"),imshow(mean_image);
