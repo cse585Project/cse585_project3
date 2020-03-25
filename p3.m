99dadc490020bced8dc0b1859cbba93dcd850a83
Yuhui Lyu <yuhuilvlu@gmail.com>
2020-03-24 23:39:30 -0400

    Update p3.m
    changes

--- p3.m
+++ p3.m
@@ -1,16 +1,30 @@
+%%
 clear all;
 % Load the image
 f = imread('RandomDisks-P10.jpg');
 % Convert RGB image to grayscale image
 g = rgb2gray(f);
+figure('Name',"Original Grayscale image"),imshow(g);
 
-% Apply median filter to grayscale image
-med_image = medfilt2(g);
+%% Apply built-in median filter on grayscale image
+% 1 Iteration
+med_image = medfilt2(g, [5,5]);
+figure('Name',"5x5 Median filter result (1 iteration)"),imshow(med_image);
 
-figure,imshow(g);
-% figure,imshow(med_image);
+% 5 Iterations
+for i = 1:4
+    med_image = medfilt2(med_image, [5,5]);
+end
+figure('Name',"5x5 Median filter result (5 iterations)"),imshow(med_image);
 
-
+%% Apply 5x5 mean filter on grayscale image
 gzero = zeros(size(g));
+% 1 Iteration
 mean_image = mean5x5(g, gzero, size(g, 1), size(g,2));
-figure,imshow(mean_image);
+figure('Name',"5x5 Mean filter result (1 iteration)"),imshow(mean_image);
+
+% 5 Iterations
+for i = 1:4
+    mean_image = mean5x5(mean_image, gzero, size(g, 1), size(g,2));
+end
+figure('Name',"5x5 Mean filter result (5 iterations)"),imshow(mean_image);
