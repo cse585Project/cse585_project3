b605d2a0984721a0a4858caf6f2b5596dbcbe4c1
Yuchen Liu <liu2529508462@gmail.com>
2020-03-26 15:15:34 -0400

    Update p3.m

--- p3.m
+++ p3.m
@@ -42,24 +42,90 @@ figure('Name',"5x5 sigma filter result with sigma 20 (5 iterations)"),imshow(sig
 % part (a)
 % K=30, g(.)=exp
 img = imread('cwheelnoise.gif');
-figure('Name','original image'),imshow(img);
-res1 = anisotopicdiff(img,5,30,1);
-figure('Name','after 5 iteration'),imshow(res1);
-res2 = anisotopicdiff(img,20,30,1);
-figure('Name','after 20 iteration'),imshow(res2);
-res3 = anisotopicdiff(img,100,30,1);
-figure('Name','after 100 iteration'),imshow(res3);
+subplot(4,3,1);
+imshow(img);
+title('original image');
+
+subplot(4,3,2);
+plot((1:256),img(:,128).','-');
+title('plot y=128 of original image');
+
+tmp = img;
+tmp((tmp>=80) & (tmp<=110))=0;
+tmp(((0<tmp)&(tmp<80)) | (tmp>110))=255;
+subplot(4,3,3);
+imshow(tmp);
+title('spoke of original image');
+
+
+res1 = anisotopicdiff(img,5,5,2);
+subplot(4,3,4);
+imshow(res1);
+title('image after 5 iteration');
+
+subplot(4,3,5);
+plot((1:256),res1(:,128).','-');
+title('plot y=128 after 5 iteration');
+
+tmp = res1;
+tmp((tmp>=80) & (tmp<=110))=0;
+tmp(((0<tmp)&(tmp<80)) | (tmp>110))=255;
+subplot(4,3,6);
+imshow(tmp);
+title('spoke of image after 5 iteration');
+
+
+res2 = anisotopicdiff(img,20,5,2);
+subplot(4,3,7);
+imshow(res2);
+title('image after 20 iteration');
+
+subplot(4,3,8);
+plot((1:256),res2(:,128).','-');
+title('plot y=128 after 20 iteration');
+
+tmp = res2;
+tmp((tmp>=80) & (tmp<=110))=0;
+tmp(((0<tmp)&(tmp<80)) | (tmp>110))=255;
+subplot(4,3,9);
+imshow(tmp);
+title('spoke of image after 20 iteration');
+
+
+res3 = anisotopicdiff(img,100,5,2);
+subplot(4,3,10);
+imshow(res3);
+title('image after 100 iteration');
+
+subplot(4,3,11);
+plot((1:256),res3(:,128).','-');
+title('plot y=128 after 100 iteration');
+
+tmp = res3;
+tmp((tmp>=80) & (tmp<=110))=0;
+tmp(((0<tmp)&(tmp<80)) | (tmp>110))=255;
+subplot(4,3,12);
+imshow(tmp);
+title('spoke of image after 100 iteration');
 
 % part (b)
 % K=30, g(.)=exp
 img = imread('cameraman.tif');
-figure('Name','original image'),imshow(img);
+subplot(2,2,1);
+imshow(img);
+title('original image');
 res1 = anisotopicdiff(img,5,30,1);
-figure('Name','after 5 iteration'),imshow(res1);
+subplot(2,2,2);
+imshow(res1);
+title('image after 5 iteration');
 res2 = anisotopicdiff(img,20,30,1);
-figure('Name','after 20 iteration'),imshow(res2);
+subplot(2,2,3);
+imshow(res2);
+title('image after 20 iteration');
 res3 = anisotopicdiff(img,100,30,1);
-figure('Name','after 100 iteration'),imshow(res3);
+subplot(2,2,4);
+imshow(res3);
+title('image after 100 iteration');
 
 %% 5x5 alpha-trimmed mean filter (alpha = 0.25)
 % 1 Iteration
